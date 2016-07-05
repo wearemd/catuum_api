67593672b3e33180009bcc9ce19db5bf61f7c9ee
awea <aweaoftheworld@gmail.com>
2016-07-05 12:16:06 +0200

    Update in config/prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :catuum_api, CatuumApi.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "cats.wearemd.com", port: {:system, "PORT"}],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [host: "cats.wearemd.com"]
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -48,7 +47,7 @@ config :logger, level: :info
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start the server for all endpoints:
 #
-#     config :phoenix, :serve_endpoints, true
+config :phoenix, :serve_endpoints, true
 #
 # Alternatively, you can configure exactly which server to
 # start per endpoint:
